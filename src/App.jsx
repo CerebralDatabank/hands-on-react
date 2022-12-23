1c4f7f6fd4727011824c7ad402f3680fe66bcc66
Gopal Othayoth <Gopal.Othayoth@outlook.com>
2022-12-23 19:57:43 +0000

    1-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 function App() {
+  const name = "StarGazers";
+  const desc = "intergalactic alliance";
+  let underlined = {
+    textDecoration: "underline"
+  };
   return (
     <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+      <h1>Meet the <span style={{color: "#1E90FF", fontWeight: "bold"}}>{name}</span></h1>
+      <p>Members of an <b style={underlined}>{desc}</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+      <p><button onClick={() => alert("Yay button!!")}>Button!</button></p>
     </hgroup>
   )
 }
