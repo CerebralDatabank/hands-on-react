c53191d230a534641a4b75627a39b137f6367247
Gopal Othayoth <Gopal.Othayoth@outlook.com>
2022-12-24 02:50:14 +0000

    2-4

--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -1,4 +1,4 @@
-export default ({ member, handleClose }) => {
+export default ({ member, handleClose, memberJump }) => {
   return (
     <dialog id="modal-member" open>
       <article>
@@ -19,6 +19,10 @@ export default ({ member, handleClose }) => {
             <hgroup>
               <h1>{member.name}</h1>
               <p>{member.bio}</p>
+              <hgroup style={{marginTop: "10px"}}>
+                <button style={{width: "140px", height: "70px", display: "inline-block"}} onClick={() => memberJump(-1)}>&larr; Previous</button>
+                <button style={{width: "140px", height: "70px", display: "inline-block", marginLeft: "20px"}} onClick={() => memberJump(1)}>Next &rarr;</button>
+              </hgroup>
             </hgroup>
           </div>
         </hgroup>
